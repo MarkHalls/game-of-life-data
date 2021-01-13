6e255363f6bd87b1c6f3eb6393202998cd72de62
Mark Halls <mark.halls@gmail.com>
2021-01-13 14:26:34 -0700

    Add naive random game generator

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,4 +1,4 @@
-import { update, Cell } from "./index";
+import { create, next, Cell } from "./game";
 
 it(`returns a list of living cell coords`, () => {
   const originalLivingCells: Cell[] = [
@@ -7,11 +7,17 @@ it(`returns a list of living cell coords`, () => {
     [0, 2],
   ];
 
-  const newLivingCells = update(originalLivingCells);
+  const newLivingCells = next(originalLivingCells);
 
   expect(newLivingCells).toEqual([
     [-1, 1],
     [0, 1],
     [1, 1],
   ]);
+});
+
+it(`returns a list of random points`, () => {
+  const cellArr = create({ xMin: 0, xMax: 10, yMin: 0, yMax: 10, density: 1 });
+
+  expect(cellArr.length).toBe(100);
 });
--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,18 @@
+import seedrandom from "seedrandom";
+
 import { countNeighbors, calcLivingCells } from "./utils";
 
 export type Cell = [number, number];
 
-const update = (livingCells: readonly Cell[]): Cell[] => {
+export type GameGen = {
+  xMax: number;
+  xMin: number;
+  yMax: number;
+  yMin: number;
+  density: number;
+};
+
+const next = (livingCells: readonly Cell[]): Cell[] => {
   const neighborCount = countNeighbors(livingCells);
   const living = calcLivingCells(livingCells, neighborCount);
 
@@ -12,4 +22,59 @@ const update = (livingCells: readonly Cell[]): Cell[] => {
   });
 };
 
-export { update };
+const generateRandomPoint = (game: GameGen, rng: seedrandom.prng): Cell => {
+  const { xMax, xMin, yMax, yMin } = game;
+
+  const xCenter = xMax - xMin;
+  const yCenter = yMax - yMin;
+
+  const radius = xCenter;
+
+  const randInRadius = radius * Math.sqrt(rng());
+
+  const randCircumference = 2 * Math.PI * rng();
+
+  const x = (randInRadius * Math.cos(randCircumference)) / Math.cos(yCenter);
+
+  const y = randInRadius * Math.sin(randCircumference);
+
+  const cell: Cell = [x + xCenter, y + yCenter];
+
+  return cell;
+};
+
+const create = (game: GameGen): Cell[] => {
+  return createSeeded(game, seedrandom.alea().toString());
+};
+
+const createSeeded = (game: GameGen, seed: string): Cell[] => {
+  const rng = seedrandom.alea(seed);
+
+  return naiveRandomCoords(game, rng);
+};
+
+//generates all possible coords, shuffles and returns a truncated list based on density requested.
+const naiveRandomCoords = (game: GameGen, random: seedrandom.prng): Cell[] => {
+  const { xMax, xMin, yMax, yMin, density } = game;
+
+  const cellSet: Set<Cell> = new Set();
+
+  for (let x = xMin; x < xMax; x++) {
+    for (let y = yMin; y < yMax; y++) {
+      cellSet.add([x, y]);
+    }
+  }
+
+  const cellArr: Cell[] = [...cellSet];
+
+  // shuffle
+  for (let i = cellArr.length - 1; i > 0; i--) {
+    let j = Math.floor(random() * (i + 1));
+
+    [cellArr[i], cellArr[j]] = [cellArr[j], cellArr[i]];
+  }
+
+  return cellArr.slice(0, Math.floor(cellArr.length * density));
+};
+
+export { create, createSeeded, next, generateRandomPoint, naiveRandomCoords };
