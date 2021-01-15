28f671ec639b35e5e0da00d5ae5f9c21d9e821a4
Mark Halls <mark.halls@gmail.com>
2021-01-15 16:19:37 -0700

    Remove extra tests

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,6 +1,12 @@
 import { create, next, Cell } from "./game";
 
-it(`returns a list of living cell coords`, () => {
+it(`returns a list of random points`, () => {
+  const cellArr = create({ xMin: 0, xMax: 10, yMin: 0, yMax: 10, density: 1 });
+
+  expect(cellArr.length).toBe(100);
+});
+
+it(`returns the next set of data points`, () => {
   const originalLivingCells: Cell[] = [
     [0, 0],
     [0, 1],
@@ -14,10 +20,4 @@ it(`returns a list of living cell coords`, () => {
     [0, 1],
     [1, 1],
   ]);
-});
-
-it(`returns a list of random points`, () => {
-  const cellArr = create({ xMin: 0, xMax: 10, yMin: 0, yMax: 10, density: 1 });
-
-  expect(cellArr.length).toBe(100);
 });
